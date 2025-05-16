9f1c7c165d0f04e614cde30e0d469386af7afe8b
Devin <112581672+dskipps@users.noreply.github.com>
2025-05-15 22:32:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ db_name = os.environ.get('DB_NAME')
 LAMBDA_ENDPOINT = 'https://hfvco24x7b.execute-api.us-east-2.amazonaws.com/default/logTaskLambda'
 
 # Set up AWS credentials
-def get_db_secret(secret_name, region_name='us-east-2'):
+def get_db_secret(secret_name, region_name='us-west-1'):
     client = boto3.client('secretsmanager', region_name=region_name)
     get_secret_value_response = client.get_secret_value(SecretId=secret_name)
     
